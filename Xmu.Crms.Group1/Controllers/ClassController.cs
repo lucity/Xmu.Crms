d0f3fec08656f3e61d06b766875f5450ee5712e7
lucity <34018426+lucity@users.noreply.github.com>
2017-12-26 18:58:07 +0800

    Update

--- Xmu.Crms.Group1/Controllers/ClassController.cs
+++ Xmu.Crms.Group1/Controllers/ClassController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Xmu.Crms.Shared.Exceptions;
+using Xmu.Crms.Shared.Models;
 using Xmu.Crms.Shared.Service;
 
 namespace Xmu.Crms.Group1.Controllers
@@ -36,7 +38,7 @@ namespace Xmu.Crms.Group1.Controllers
         //根据班级id以及seminarid获取学生名单
         //GET api/class/{classId}
         [HttpGet("{classid}/attendance")]
-        public JsonResult getAttendanceById(long classid, [FromQuery]bool showLate, [FromQuery]long seminarid)
+        public IActionResult getAttendanceById(long classid, [FromQuery]bool showLate, [FromQuery]long seminarid)
         {
             if (showLate)
             {
@@ -47,6 +49,51 @@ namespace Xmu.Crms.Group1.Controllers
             {
                 var students = userService.ListPresentStudent(seminarid, classid);
                 return Json(students);
+            }
+        }
+
+
+        //根据classid和seminarid老师开始签到
+        //POST api/class/{classId}/startclass
+        [HttpPost("{classid}/startclass")]
+        public IActionResult startClass(long classid,[FromBody]dynamic json)
+        {
+            Location location = new Location()
+            {
+                ClassInfo = classService.GetClassByClassId(classid),
+                Latitude = json.latitude,
+                Longitude=json.longitude,
+                Seminar=json.seminarid,
+                Status=1
+            };
+            classService.CallInRollById(location);
+            return Json(new { status = 200 });
+        }
+
+        //根据classid和seminarid老师结束签到
+        //POST api/class/{classId}/endclass
+        [HttpPost("{classid}/endclass")]
+        public IActionResult endClass(long classid, [FromBody]dynamic json)
+        {
+            classService.EndCallRollById(json.seminarid, classid);
+            return Json(new { status = 200 });
+        }
+
+        //根据classid和seminarid获取当前签到情况
+        //GET api/class/{classid}/getstatus
+        [HttpGet("{classid}/getstatus")]
+        public IActionResult getStatus(long classid,[FromBody]dynamic json)
+        {
+            try
+            {
+                Location location = classService.GetCallStatusById(json.seminarid, classid);
+                if(location==null)
+                    return Json(new { callstatus = 0 });
+                return Json(new { callstatus = location.Status });
+            }
+            catch(SeminarNotFoundException e)
+            {
+                return Json(new { callstatus = 0 });
             }
         }
     }
